9f58a9f72ca881d8dbded4f0608e20327f0a7881
Niels Basjes <niels@basjes.nl>
2023-10-21 13:35:44 +0200

    Working combination

---
+++ flutter-client/lib/client.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+ValueNotifier<GraphQLClient> getGraphQLCLient() {
+  const String baseLink = 'epoch.klokko.nl/graphql';
+  // const String _token = "Dummy value";
+
+  final Link httpLink =
+  HttpLink(
+    'https://$baseLink',
+  );// .concat(AuthLink(getToken: () => _token));
+
+  final WebSocketLink websocketLink =
+  WebSocketLink(
+    'wss://$baseLink',
+    subProtocol: GraphQLProtocol.graphqlTransportWs,
+    config: const SocketClientConfig(
+      autoReconnect: true,
+      inactivityTimeout: Duration(seconds: 30),
+      // initialPayload: () => async {
+      //   return {
+      //     'headers': {'Authorization': _token},
+      //   };
+      // },
+    ),
+  );
+
+  final Link link = Link.split((request) => request.isSubscription, websocketLink, httpLink);
+
+   return ValueNotifier(
+    GraphQLClient(
+      link: link,
+      cache: GraphQLCache(), // The default store is the InMemoryStore, which does NOT persist to disk
+    ),
+  );
+
+}
--- flutter-client/lib/main.dart
+++ flutter-client/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:intl/intl.dart';
+
+import 'client.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,43 +14,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return GraphQLProvider(
+        client: getGraphQLCLient(),
+        child: MaterialApp(
+          title: 'Flutter GraphQL Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+            useMaterial3: true,
+          ),
+          home: const MyHomePage(title: 'Flutter GraphQL Demo Home Page'),
+        ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +38,92 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int year = 0;
+  int month = 0;
+  int day = 0;
+  int hour = 0;
+  int minute = 0;
+  int second = 0;
+  int millisecond = 0;
+  String iso = "Nothing yet";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final subscriptionDocument = gql(
+    r'''
+      subscription {
+        now {
+          year
+          month
+          day
+          hour
+          minute
+          second
+          millisecond
+          iso
+        }
+      }
+    ''',
+  );
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: Subscription(
+          options: SubscriptionOptions(
+            document: subscriptionDocument,
+          ),
+          builder: (result) {
+            if (result.hasException) {
+              return Text(result.exception.toString());
+            }
+            if (result.isLoading) {
+              return Center(
+                child: Column(
+                children: <Widget>[
+                  const Text('Is Loading:'),
+                  Text('$result'),
+                  const CircularProgressIndicator(),
+                  ],
+                )
+              );
+            }
+            if (result.data == null) {
+              return const Center(
+                  child: Text('There is no data ?!?!'),
+              );
+            }
+            var payload = result.data!['now'];
+            year         = payload['year'];
+            month        = payload['month'];
+            day          = payload['day'];
+            hour         = payload['hour'];
+            minute       = payload['minute'];
+            second       = payload['second'];
+            millisecond  = payload['millisecond'];
+            iso          = payload['iso'];
+
+            var f2 = NumberFormat("00", "en_US");
+            var f3 = NumberFormat("000", "en_US");
+            var f4 = NumberFormat("0000", "en_US");
+
+            // ResultAccumulator is a provided helper widget for collating subscription results.
+            // careful though! It is stateful and will discard your results if the state is disposed
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Current time:'),
+                Text(f4.format(year)+'-'+f2.format(month)+'-'+f2.format(day)+' '+f2.format(hour)+':'+f2.format(minute)+':'+f2.format(second)+'.'+f3.format(millisecond),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            );
+          }
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    )
     );
   }
 }
