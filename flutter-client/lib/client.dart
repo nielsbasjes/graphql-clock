4edf756504561726f4542b0eefd067007384f824
Niels Basjes <niels@basjes.nl>
2023-10-23 15:59:15 +0200

    Widget works also in released version.

--- flutter-client/lib/client.dart
+++ flutter-client/lib/client.dart
@@ -1,17 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 GraphQLClient getGraphQLCLient() {
   const String baseLink = 'epoch.klokko.nl/graphql';
   // const String _token = "Dummy value";
 
-  final Link httpLink =
-  HttpLink(
+  final Link httpLink = HttpLink(
     'https://$baseLink',
-  );// .concat(AuthLink(getToken: () => _token));
+  ); // .concat(AuthLink(getToken: () => _token));
 
-  final WebSocketLink websocketLink =
-  WebSocketLink(
+  final WebSocketLink websocketLink = WebSocketLink(
     'wss://$baseLink',
     subProtocol: GraphQLProtocol.graphqlTransportWs,
     config: const SocketClientConfig(
@@ -25,19 +22,18 @@ GraphQLClient getGraphQLCLient() {
     ),
   );
 
-  final Link link = Link.split((request) => request.isSubscription, websocketLink, httpLink);
+  final Link link =
+      Link.split((request) => request.isSubscription, websocketLink, httpLink);
 
-   return
-    GraphQLClient(
-      link: link,
-      cache: GraphQLCache(), // The default store is the InMemoryStore, which does NOT persist to disk
-      defaultPolicies: DefaultPolicies(
+  return GraphQLClient(
+    link: link,
+    cache: GraphQLCache(),
+    // The default store is the InMemoryStore, which does NOT persist to disk
+    defaultPolicies: DefaultPolicies(
         query: Policies(
-          fetch: FetchPolicy.networkOnly,
-          error: ErrorPolicy.none,
-          cacheReread: CacheRereadPolicy.ignoreAll,
-        )
-      ),
-    );
-
+      fetch: FetchPolicy.networkOnly,
+      error: ErrorPolicy.none,
+      cacheReread: CacheRereadPolicy.ignoreAll,
+    )),
+  );
 }
